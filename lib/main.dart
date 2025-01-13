2004ca5d28161d889e9fe757d1d115c7d7a4de3b
uche-hub <ucj.theuche@gmail.com>
2025-01-13 23:41:42 +0100

    Created Start Screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,16 @@ import 'package:quiz_app/start_screen.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: StartScreen(),
+        body: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+                const Color.fromARGB(255, 67, 8, 169),
+                const Color.fromARGB(255, 119, 48, 243)
+              ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+            ),
+            child: StartScreen()),
       ),
     ),
   );
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,6 +5,37 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text("Start Screen");
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          SizedBox(
+            height: 80,
+          ),
+          Text(
+            "Learn Flutter the fun way",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            child: Text(
+              'Start Quiz',
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
