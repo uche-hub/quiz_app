2132fe7aeaf11e9e1e4903d607fe82897af6894d
uche-hub <ucj.theuche@gmail.com>
2025-01-18 13:05:36 +0100

    Made answer clickable and automatically move to the next question

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/aanswer_button.dart';
+import 'package:quiz_app/data/queestions.dart';
 
 class Question extends StatefulWidget {
   const Question({super.key});
@@ -11,24 +12,43 @@ class Question extends StatefulWidget {
 }
 
 class _QuestionState extends State<Question> {
+  var currentQuestionIndex = 0;
+
+  void anserQuestion() {
+    // currentQuestionIndex = currentQuestionIndex + 1;
+    setState(() {
+      currentQuestionIndex += 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[currentQuestionIndex];
+
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'The Question.....',
-            style: TextStyle(color: Colors.white),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          AanswerButton(answerText: "Answer 1", onTap: () {}),
-          AanswerButton(answerText: "Answer 2", onTap: () {}),
-          AanswerButton(answerText: "Answer 3", onTap: () {})
-        ],
+      child: Container(
+        margin: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AanswerButton(
+                answerText: answer,
+                onTap: anserQuestion,
+              );
+            }),
+          ],
+        ),
       ),
     );
   }
